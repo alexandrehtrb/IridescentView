7409224fb4636ff99ccc003f36202ef84c0a7a52
Alexandre Bonfitto <alexandre.bonfitto@dextra-sw.com>
2021-01-16 20:34:29 -0300

    Arrumando nomes das pastas do Xamarin.Android

---
+++ XamarinAndroid/Br.AlexandreHtrb.IridescentViewApp/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Br.AlexandreHtrb.IridescentViewApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
